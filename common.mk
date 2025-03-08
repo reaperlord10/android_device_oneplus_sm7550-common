cc9bdc67adcc0d3e63188da2dcba254b0973db3b
Bruno Martins <bgcngm@gmail.com>
2025-03-08 11:30:58 +0000

    sm7550-common: Move to reference fastboot aidl service
    Oneplus is also using it since sm8650.
    
    Change-Id: I206d23420952244c40cc73d384c2e8089254f4ae

--- common.mk
+++ common.mk
@@ -127,7 +127,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
 # Fastboot
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl.custom \
+    android.hardware.fastboot-service.example_recovery \
     fastbootd
 
 PRODUCT_COPY_FILES += \
