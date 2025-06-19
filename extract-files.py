fecbebc780f9e8dcb4ac7a5c4ffb776e05f86294
johnmart19 <johnivan19999@gmail.com>
2025-06-19 08:11:24 +0300

    sm7550-common: Add libaudioclient_shim for libwfdservice
    Android 16 compilation error:
    libwfdservice.so: error: Unresolved symbol: _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t
    libwfdservice.so: note:
    libwfdservice.so: note: Some dependencies might be changed, thus the symbol(s) above cannot be resolved.
    libwfdservice.so: note: Please re-build the prebuilt file: "out/soong/.intermediates/vendor/oneplus/sm7550-common/libwfdservice/android_arm64_armv8-2a-dotprod_cortex-a76_shared/libwfdservice.so".
    libwfdservice.so: note:
    libwfdservice.so: note: If this is a new prebuilt file and it is designed to have unresolved symbols, add one of the following properties:
    libwfdservice.so: note:   Android.bp: allow_undefined_symbols: true,
    libwfdservice.so: note:   Android.mk: LOCAL_ALLOW_UNDEFINED_SYMBOLS := true

--- extract-files.py
+++ extract-files.py
@@ -91,6 +91,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
