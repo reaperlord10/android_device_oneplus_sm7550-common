fdfae5a815d6e6f86fecd15ce71555ecbbabb5ea
Arian <arian.kulmer@web.de>
2025-07-06 18:32:00 +0200

    sm7550-common: Enable ELF checks for audio blobs
    We converted the audio hal targets
    
    Change-Id: I6b46b5c87a2c731241a7c2e34f4271545e2b61e0

--- common.mk
+++ common.mk
@@ -42,13 +42,11 @@ PRODUCT_PACKAGES += \
     libagm_pcm_plugin \
     libbatterylistener \
     libfmpal \
-    libats \
     libhfp_pal \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    vendor.qti.hardware.AGMIPC@1.0-impl \
     lib_bt_aptx \
     lib_bt_ble \
     lib_bt_bundle \
@@ -213,9 +211,6 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.base-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.base-arm64.policy \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy
-
-PRODUCT_PACKAGES += \
-    libpalclient
 
 # Memtrack
 PRODUCT_PACKAGES += \
--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -68,15 +67,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.imsrtpservice@3.1',
     ): lib_fixup_vendor_suffix,
-    (
-        'libagmclient',
-        'libpalclient',
-        'libar-acdb',
-        'libar-gsl',
-        'liblx-osal',
-        'libats',
-        'vendor.qti.hardware.AGMIPC@1.0-impl',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
