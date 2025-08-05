55b17ebe22e1adaae3e161d10f61729bf158d64f
dianlujitao <dianlujitao@lineageos.org>
2025-08-05 12:57:12 +0800

    sm7550-common: Support IFAA fingerprint payment
    Change-Id: I3961dc0993e995b4f77b55aace8ca8625bd3f2c7

--- common.mk
+++ common.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    IFAAService
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
