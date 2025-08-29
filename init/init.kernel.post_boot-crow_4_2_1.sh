e6a776f59c7669b838e0cf1cf147dd15ab68017b
reaperlord10 <reaperlordsgaming@gmail.com>
2025-08-29 19:43:40 +0530

    sm7550-common: Adaptation for Lunaris

--- init/init.kernel.post_boot-crow_4_2_1.sh
+++ init/init.kernel.post_boot-crow_4_2_1.sh
@@ -31,10 +31,6 @@ echo 255 > /proc/sys/walt/sched_util_busy_hysteresis_enable_cpus
 echo 8500000 8500000 8500000 8500000 5000000 5000000 2000000 > /proc/sys/walt/sched_util_busy_hyst_cpu_ns
 echo 1 1 1 1 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
 
-# cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
-
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
 
--- init/init.kernel.post_boot-kalama.sh
+++ init/init.kernel.post_boot-kalama.sh
@@ -112,10 +112,6 @@ echo $silver_early_upmigrate $gold_early_upmigrate > /proc/sys/walt/sched_early_
 # binder activity tasks
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
-# cpuset parameters
-echo 0-2 > /dev/cpuset/background/cpus
-echo 0-2 > /dev/cpuset/system-background/cpus
-
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
 
